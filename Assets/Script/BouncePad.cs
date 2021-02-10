a7bd347cf83e9661df818676331ee30597256b56
Robert Garcia-Allen <s199030@students.aie.edu>
2021-02-10 12:53:13 -0800

    workng on bounce pad

--- Assets/Script/BouncePad.cs
+++ Assets/Script/BouncePad.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BouncePad : MonoBehaviour
 {
+    public Vector3 movement;
+    private Transform otherTransform;
     #region MonoBehaviour Callbacks
     // Start is called before the first frame update
     void Start()
@@ -14,7 +16,12 @@ public class BouncePad : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //otherTransform.position += movement;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        otherTransform = other.gameObject.transform;
+        Debug.Log("Hit");
     }
     #endregion
 }
